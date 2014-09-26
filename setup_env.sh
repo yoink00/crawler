f2f626206535ce179adf029f9b5b8911b94ed494
Stuart Wallace <stuart@wapbot.co.uk>
2014-09-26 23:07:31 +0100

    Tweaked install of go-cover. Added goquery package

--- setup_env.sh
+++ setup_env.sh
@@ -7,13 +7,17 @@
 # Check for the presence of 'go tool cover'
 ##
 go tool cover > /dev/null 2>&1
-if [ $? -ne 0 ]; then
+RET=$?
+if [ $RET -ne 0 -a $RET -ne 1 ]; then
 	echo "Go coverage tool does not appear to be installed."
 	echo "Attempting to install it..."
 	# This needs to run as root because it goes and installs
 	# things in naughty places. I'd rather it didn't but hey...
 	# I do my development in a BSD jail so I don't care.
 	sudo -E go get -u code.google.com/p/go.tools/cmd/cover
+	# Restore the ownership of code.google.com
+	sudo -E /usr/sbin/chown -R $USER:$GROUP ./src/code.google.com
 fi
 go get -u github.com/nsf/gocode
 go get -u github.com/smartystreets/goconvey
+go get -u github.com/puerkitobio/goquery
